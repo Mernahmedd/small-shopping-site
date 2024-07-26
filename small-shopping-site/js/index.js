f80d6ddd0e7a0aa5b325616dff1d4acc6cd1c202
Merna <94222075+Mernahmedd@users.noreply.github.com>
2024-07-27 01:44:34 +0300

    upload small shopping site

---
+++ small-shopping-site/js/index.js
@@ -0,0 +1,13 @@
+document.querySelector(".grid").addEventListener("click", function () {
+  document.querySelector(".list").classList.remove("active");
+  document.querySelector(".grid").classList.add("active");
+  document.querySelector(".products-area").classList.add("gridView");
+  document.querySelector(".products-area").classList.remove("tableView");
+});
+
+document.querySelector(".list").addEventListener("click", function () {
+  document.querySelector(".list").classList.add("active");
+  document.querySelector(".grid").classList.remove("active");
+  document.querySelector(".products-area").classList.remove("gridView");
+  document.querySelector(".products-area").classList.add("tableView");
+});
